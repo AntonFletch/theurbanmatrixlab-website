0dd8a791932ae5fe037ec5827204fc4f75a542ad
AntonFletch <TheUrbanMatrixLab@outlook.com>
2026-07-21 10:57:07 -0600

    Update Supabase anon key in supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://xdaxbueyftyljsrhabpx.supabase.co";
 
-const SUPABASE_ANON_KEY = "PASTE_YOUR_PUBLISHABLE_ANON_KEY_HERE";
+const SUPABASE_ANON_KEY = "sb_publishable_eZ5ydFnC0SESdeT9Up_-0A_Mez9ljvZ";
 
 const supabase = window.supabase.createClient(
   SUPABASE_URL,
