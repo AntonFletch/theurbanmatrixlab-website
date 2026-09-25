fb34c2445d160c550b0a97cac539b7176173079a
AntonFletch <TheUrbanMatrixLab@outlook.com>
2026-09-24 19:43:33 -0600

    Hide public script: supabase.js

--- supabase.js
+++ supabase.js
@@ -1,3 +1,1 @@
-const UMX_SUPABASE_URL='https://xdaxbueyftyljsrhabpx.supabase.co';
-const UMX_SUPABASE_KEY='sb_publishable_eZ5ydFnC0SESdeT9Up_-0A_Mez9ljvZ';
-const umxDb=window.supabase.createClient(UMX_SUPABASE_URL,UMX_SUPABASE_KEY);
+/* Public access temporarily disabled. */
